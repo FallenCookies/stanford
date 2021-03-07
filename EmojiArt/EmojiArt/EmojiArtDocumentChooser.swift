565a72f72e2d8e81825f9ff87511a0a0e7f130a7
Vlad Zamaev <vlad@MacBook-Pro-Vlad.local>
2021-03-07 17:18:42 +0300

    added enroute demo

---
+++ EmojiArt/EmojiArt/EmojiArtDocumentChooser.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtDocumentChooser.swift
+//  EmojiArt
+//
+//  Created by Vlad Zamaev on 11.01.2021.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiArtDocumentChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentChooser()
+    }
+}
+
